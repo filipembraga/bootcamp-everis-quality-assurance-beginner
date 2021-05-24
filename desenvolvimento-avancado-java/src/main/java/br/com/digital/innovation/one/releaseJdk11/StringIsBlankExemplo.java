4b062843891ff2c09018832f07f56192b123da3a
Filipe Braga <lipebraga16@hotmail.com>
2021-05-24 02:30:09 -0300

    Novas funcionalidades Jdk 10 e Jdk 11

---
+++ desenvolvimento-avancado-java/src/main/java/br/com/digital/innovation/one/releaseJdk11/StringIsBlankExemplo.java
@@ -0,0 +1,9 @@
+package br.com.digital.innovation.one.releaseJdk11;
+
+public class StringIsBlankExemplo {
+    public static void main(String[] args) {
+        String espaco = "                                                            ";
+        System.out.println(espaco.isBlank());
+    }
+
+}
